e701c328ad91d09a402b59a942388a9ecbeee7f8
SE Zeltmann <steven.zeltmann@lbl.gov>
2020-02-01 13:00:48 -0800

    setup.py fixes and PEP8 compliance

--- cyTV4D/cyTV4D.py
+++ cyTV4D/cyTV4D.py
@@ -4,48 +4,48 @@ import numpy as np
 from tqdm import tqdm
 
 def denoise4D(datacube, lam, mu, iterations=75):
-	'''
-	Perform Proximal Anisotropic Total Variational denoising on a 4D datacube
+    '''
+    Perform Proximal Anisotropic Total Variational denoising on a 4D datacube
 
-	Arguments:
-	datacube		(np.array) a C-contiguous 4D numpy array. Must be float32 or float64 dtype
-	lam				(np.array) TV weights for each dimension. Must be same dtype as datacube
-	mu 				(float) TV weighting parameter
-	iterations		(int) number of iterations to perform TV update step
+    Arguments:
+    datacube        (np.array) a C-contiguous 4D numpy array. Must be float32 or float64 dtype
+    lam             (np.array) TV weights for each dimension. Must be same dtype as datacube
+    mu              (float) TV weighting parameter
+    iterations      (int) number of iterations to perform TV update step
 
-	The algorithm used is an extension of that shown in this paper:
-	Jia, Rong-Qing, and Hanqing Zhao. "A fast algorithm for the total variation model of image denoising."
-	Advances in Computational Mathematics 33.2 (2010): 231-241.
-	'''
+    The algorithm used is an extension of that shown in this paper:
+    Jia, Rong-Qing, and Hanqing Zhao. "A fast algorithm for the total variation model of image denoising."
+    Advances in Computational Mathematics 33.2 (2010): 231-241.
+    '''
 
-	assert datacube.dtype in (np.float32, np.float64), "datacube must be floating point datatype."
+    assert datacube.dtype in (np.float32, np.float64), "datacube must be floating point datatype."
 
-	assert lam.dtype == datacube.dtype, "Lambda must have same dtype as datacube."
+    assert lam.dtype == datacube.dtype, "Lambda must have same dtype as datacube."
 
-	assert datacube.flags['C_CONTIGUOUS'], "datacube must be C-contiguous. Try np.ascontiguousarray(datacube) on the array"
+    assert datacube.flags['C_CONTIGUOUS'], "datacube must be C-contiguous. Try np.ascontiguousarray(datacube) on the array"
 
-	lambdaInv = 1. / lam
-	lam_mu = (lam / mu).astype(datacube.dtype)
+    lambdaInv = 1. / lam
+    lam_mu = (lam / mu).astype(datacube.dtype)
 
-	assert np.all(lam_mu < (1. / 8.)) & np.all(lam_mu > 0), "Parameters must satisfy 0 < λ/μ < 1/8"
+    assert np.all(lam_mu < (1. / 8.)) & np.all(lam_mu > 0), "Parameters must satisfy 0 < λ/μ < 1/8"
 
-	# allocate memory for the accumulators and the output datacube
+    # allocate memory for the accumulators and the output datacube
 
-	acc1 = np.zeros_like(datacube)
-	acc2 = np.zeros_like(datacube)
-	acc3 = np.zeros_like(datacube)
-	acc4 = np.zeros_like(datacube)
+    acc1 = np.zeros_like(datacube)
+    acc2 = np.zeros_like(datacube)
+    acc3 = np.zeros_like(datacube)
+    acc4 = np.zeros_like(datacube)
 
-	recon = np.zeros_like(datacube)
+    recon = np.zeros_like(datacube)
 
-	for i in tqdm(range(int(iterations))):
-		# update accumulators
-		accumulator_update(recon, acc1, 0, lambdaInv[0])
-		accumulator_update(recon, acc2, 1, lambdaInv[1])
-		accumulator_update(recon, acc3, 2, lambdaInv[2])
-		accumulator_update(recon, acc4, 3, lambdaInv[3])
+    for i in tqdm(range(int(iterations))):
+        # update accumulators
+        accumulator_update(recon, acc1, 0, lambdaInv[0])
+        accumulator_update(recon, acc2, 1, lambdaInv[1])
+        accumulator_update(recon, acc3, 2, lambdaInv[2])
+        accumulator_update(recon, acc4, 3, lambdaInv[3])
 
-		# update reconstruction
-		datacube_update(datacube, recon, acc1, acc2, acc3, acc4, lam_mu)
+        # update reconstruction
+        datacube_update(datacube, recon, acc1, acc2, acc3, acc4, lam_mu)
 
-	return recon
+    return recon
--- setup.py
+++ setup.py
@@ -4,24 +4,30 @@ from Cython.Build import cythonize
 import platform
 import os
 
-extra_link_args = ['-fopenmp']
-extra_compile_args = ['-fopenmp']
+if platform.system() == 'Windows':
+    extra_compile_args = ['/openmp']
+    extra_link_args = ['/openmp']
+else:
+    extra_link_args = ['-fopenmp']
+    extra_compile_args = ['-fopenmp']
 
-if platform.system == 'Darwin':
-	# we are on a Mac, link to the Homebrew installation of llvm
-	extra_link_args.append('-lgomp')
-	extra_link_args.append('-Wl,-rpath,/usr/local/opt/gcc/lib/gcc/9/')
-	# use the Homebrew gcc
-	os.environ['CC'] = 'gcc-9'
+if platform.system() == 'Darwin':
+    # we are on a Mac, link to the Homebrew installation of llvm
+    extra_link_args.append('-lgomp')
+    extra_link_args.append('-Wl,-rpath,/usr/local/opt/gcc/lib/gcc/9/')
+    # use the Homebrew gcc
+    os.environ['CC'] = 'gcc-9'
 
-ext_modules = [Extension('cyTV4D.tv4d_utils', ['cyTV4D/tv4d_utils.pyx'], extra_compile_args=extra_compile_args,
-				extra_link_args=extra_link_args)]
+ext_modules = [Extension(
+    'cyTV4D.tv4d_utils', ['cyTV4D/tv4d_utils.pyx'],
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args)]
 
 setup(
-	name='cyTV4D',
-	version='0.0.1',
-	author="SE Zeltmann",
-	author_email="steven.zeltmann@lbl.gov",
-	packages=['cyTV4D'],
+    name='cyTV4D',
+    version='0.0.1',
+    author="SE Zeltmann",
+    author_email="steven.zeltmann@lbl.gov",
+    packages=['cyTV4D'],
     ext_modules=cythonize(ext_modules)
 )
