cde5a0815e8c4242e3ef96aa80613ddf0b9f001d
SE Zeltmann <steven.zeltmann@lbl.gov>
2020-02-21 13:49:23 -0800

    format with black

--- cyTV4D/cyTV4D.py
+++ cyTV4D/cyTV4D.py
@@ -10,8 +10,10 @@ import psutil
 from tabulate import tabulate
 
 
-def denoise4D(datacube, lam, mu, iterations=75, BC_mode=2, FISTA=False, reference_data=None):
-    '''
+def denoise4D(
+    datacube, lam, mu, iterations=75, BC_mode=2, FISTA=False, reference_data=None
+):
+    """
     Perform Proximal Anisotropic Total Variational denoising on a 4D datacube
 
     Arguments:
@@ -31,32 +33,52 @@ def denoise4D(datacube, lam, mu, iterations=75, BC_mode=2, FISTA=False, referenc
     The algorithm used is an extension of that shown in this paper:
     Jia, Rong-Qing, and Hanqing Zhao. "A fast algorithm for the total variation model of image denoising."
     Advances in Computational Mathematics 33.2 (2010): 231-241.
-    '''
+    """
 
-    assert datacube.dtype in (np.float32, np.float64), "datacube must be floating point datatype."
+    assert datacube.dtype in (
+        np.float32,
+        np.float64,
+    ), "datacube must be floating point datatype."
 
     assert lam.dtype == datacube.dtype, "Lambda must have same dtype as datacube."
 
-    assert datacube.flags['C_CONTIGUOUS'], "datacube must be C-contiguous. Try np.ascontiguousarray(datacube) on the array"
+    assert datacube.flags[
+        "C_CONTIGUOUS"
+    ], "datacube must be C-contiguous. Try np.ascontiguousarray(datacube) on the array"
 
-    lambdaInv = 1. / lam
+    lambdaInv = 1.0 / lam
     lam_mu = (lam / mu).astype(datacube.dtype)
 
-    assert np.all(lam_mu < (1. / 8.)) & np.all(lam_mu > 0), "Parameters must satisfy 0 < λ/μ < 1/8"
+    assert np.all(lam_mu < (1.0 / 8.0)) & np.all(
+        lam_mu > 0
+    ), "Parameters must satisfy 0 < λ/μ < 1/8"
     try:
-        print(f"λ/μ ≈ [1/{mu/lam[0]:.0f}, 1/{mu/lam[1]:.0f}, 1/{mu/lam[2]:.0f}, 1/{mu/lam[3]:.0f}]")
+        print(
+            f"λ/μ ≈ [1/{mu/lam[0]:.0f}, 1/{mu/lam[1]:.0f}, 1/{mu/lam[2]:.0f}, 1/{mu/lam[3]:.0f}]"
+        )
     except Exception:
-        print("I tried to print with pretty characters but your system doesn't like Unicode...")
+        print(
+            "I tried to print with pretty characters but your system doesn't like Unicode..."
+        )
     # warn about memory requirements
-    print(f"Available RAM: {size(psutil.virtual_memory().available,system=alternative)}", flush=True)
+    print(
+        f"Available RAM: {size(psutil.virtual_memory().available,system=alternative)}",
+        flush=True,
+    )
     if FISTA:
-        print(f"FISTA Accelerated TV denoising will require {size(datacube.nbytes*9,system=alternative)} of RAM...", flush=True)
+        print(
+            f"FISTA Accelerated TV denoising will require {size(datacube.nbytes*9,system=alternative)} of RAM...",
+            flush=True,
+        )
     else:
-        print(f"Unaccelerated TV denoising will require {size(datacube.nbytes*5,system=alternative)} of RAM...", flush=True)
+        print(
+            f"Unaccelerated TV denoising will require {size(datacube.nbytes*5,system=alternative)} of RAM...",
+            flush=True,
+        )
 
     calculate_MSE = reference_data is not None
     if calculate_MSE:
-        MSE = np.zeros((iterations+1,), dtype=datacube.dtype)
+        MSE = np.zeros((iterations + 1,), dtype=datacube.dtype)
         MSE[0] = sum_square_error_4D(datacube, reference_data)
 
     error = np.zeros((iterations), dtype=datacube.dtype)
@@ -74,37 +96,57 @@ def denoise4D(datacube, lam, mu, iterations=75, BC_mode=2, FISTA=False, referenc
         d3 = np.zeros_like(datacube)
         d4 = np.zeros_like(datacube)
 
-        tk = 1.
+        tk = 1.0
 
     recon = datacube.copy()
 
     if FISTA:
-        for i in tqdm(range(int(iterations)), desc='FISTA Accelerated TV Denoising'):
+        for i in tqdm(range(int(iterations)), desc="FISTA Accelerated TV Denoising"):
             # update the tk factor
-            tk_new = (1 + np.sqrt(1 + 4*tk**2)) / 2
+            tk_new = (1 + np.sqrt(1 + 4 * tk ** 2)) / 2
             tk_ratio = tk / tk_new
             tk = tk_new
 
             # update accumulators
-            error[i] += accumulator_update_4D_FISTA(recon, acc1, d1, tk_ratio, 0, lambdaInv[0], BC_mode=BC_mode)
-            error[i] += accumulator_update_4D_FISTA(recon, acc2, d2, tk_ratio, 1, lambdaInv[1], BC_mode=BC_mode)
-            error[i] += accumulator_update_4D_FISTA(recon, acc3, d3, tk_ratio, 2, lambdaInv[2], BC_mode=BC_mode)
-            error[i] += accumulator_update_4D_FISTA(recon, acc4, d4, tk_ratio, 3, lambdaInv[3], BC_mode=BC_mode)
+            error[i] += accumulator_update_4D_FISTA(
+                recon, acc1, d1, tk_ratio, 0, lambdaInv[0], BC_mode=BC_mode
+            )
+            error[i] += accumulator_update_4D_FISTA(
+                recon, acc2, d2, tk_ratio, 1, lambdaInv[1], BC_mode=BC_mode
+            )
+            error[i] += accumulator_update_4D_FISTA(
+                recon, acc3, d3, tk_ratio, 2, lambdaInv[2], BC_mode=BC_mode
+            )
+            error[i] += accumulator_update_4D_FISTA(
+                recon, acc4, d4, tk_ratio, 3, lambdaInv[3], BC_mode=BC_mode
+            )
 
-            datacube_update_4D(datacube, recon, acc1, acc2, acc3, acc4, lam_mu, BC_mode=BC_mode)
+            datacube_update_4D(
+                datacube, recon, acc1, acc2, acc3, acc4, lam_mu, BC_mode=BC_mode
+            )
 
             if calculate_MSE:
                 MSE[i + 1] = sum_square_error_4D(reference_data, recon)
     else:
-        for i in tqdm(range(int(iterations)), desc='Unaccelerated TV Denoising'):
+        for i in tqdm(range(int(iterations)), desc="Unaccelerated TV Denoising"):
             # update accumulators
-            error[i] += accumulator_update_4D(recon, acc1, 0, lambdaInv[0], BC_mode=BC_mode)
-            error[i] += accumulator_update_4D(recon, acc2, 1, lambdaInv[1], BC_mode=BC_mode)
-            error[i] += accumulator_update_4D(recon, acc3, 2, lambdaInv[2], BC_mode=BC_mode)
-            error[i] += accumulator_update_4D(recon, acc4, 3, lambdaInv[3], BC_mode=BC_mode)
+            error[i] += accumulator_update_4D(
+                recon, acc1, 0, lambdaInv[0], BC_mode=BC_mode
+            )
+            error[i] += accumulator_update_4D(
+                recon, acc2, 1, lambdaInv[1], BC_mode=BC_mode
+            )
+            error[i] += accumulator_update_4D(
+                recon, acc3, 2, lambdaInv[2], BC_mode=BC_mode
+            )
+            error[i] += accumulator_update_4D(
+                recon, acc4, 3, lambdaInv[3], BC_mode=BC_mode
+            )
 
             # update reconstruction
-            datacube_update_4D(datacube, recon, acc1, acc2, acc3, acc4, lam_mu, BC_mode=BC_mode)
+            datacube_update_4D(
+                datacube, recon, acc1, acc2, acc3, acc4, lam_mu, BC_mode=BC_mode
+            )
 
             if calculate_MSE:
                 MSE[i + 1] = sum_square_error_4D(reference_data, recon)
@@ -115,8 +157,10 @@ def denoise4D(datacube, lam, mu, iterations=75, BC_mode=2, FISTA=False, referenc
         return recon, error
 
 
-def denoise3D(datacube, lam, mu, iterations=75, BC_mode=2, FISTA=False, reference_data=None):
-    '''
+def denoise3D(
+    datacube, lam, mu, iterations=75, BC_mode=2, FISTA=False, reference_data=None
+):
+    """
     Perform Proximal Anisotropic Total Variational denoising on a 3D datacube
 
     Arguments:
@@ -136,28 +180,46 @@ def denoise3D(datacube, lam, mu, iterations=75, BC_mode=2, FISTA=False, referenc
     The algorithm used is an extension of that shown in this paper:
     Jia, Rong-Qing, and Hanqing Zhao. "A fast algorithm for the total variation model of image denoising."
     Advances in Computational Mathematics 33.2 (2010): 231-241.
-    '''
+    """
 
-    assert datacube.dtype in (np.float32, np.float64), "datacube must be floating point datatype."
+    assert datacube.dtype in (
+        np.float32,
+        np.float64,
+    ), "datacube must be floating point datatype."
 
     assert lam.dtype == datacube.dtype, "Lambda must have same dtype as datacube."
 
-    assert datacube.flags['C_CONTIGUOUS'], "datacube must be C-contiguous. Try np.ascontiguousarray(datacube) on the array"
+    assert datacube.flags[
+        "C_CONTIGUOUS"
+    ], "datacube must be C-contiguous. Try np.ascontiguousarray(datacube) on the array"
 
-    lambdaInv = 1. / lam
+    lambdaInv = 1.0 / lam
     lam_mu = (lam / mu).astype(datacube.dtype)
 
-    assert np.all(lam_mu < (1. / 8.)) & np.all(lam_mu > 0), "Parameters must satisfy 0 < λ/μ < 1/8"
+    assert np.all(lam_mu < (1.0 / 8.0)) & np.all(
+        lam_mu > 0
+    ), "Parameters must satisfy 0 < λ/μ < 1/8"
     try:
         print(f"λ/μ ≈ [1/{mu/lam[0]:.0f}, 1/{mu/lam[1]:.0f}, 1/{mu/lam[2]:.0f}]")
     except Exception:
-        print("I tried to print with pretty characters but your system doesn't like Unicode...")
+        print(
+            "I tried to print with pretty characters but your system doesn't like Unicode..."
+        )
     # warn about memory requirements
-    print(f"Available RAM: {size(psutil.virtual_memory().available,system=alternative)}", flush=True)
+    print(
+        f"Available RAM: {size(psutil.virtual_memory().available,system=alternative)}",
+        flush=True,
+    )
     if FISTA:
-        print(f"FISTA Accelerated TV denoising will require {size(datacube.nbytes*7,system=alternative)} of RAM...", flush=True)
+        print(
+            f"FISTA Accelerated TV denoising will require {size(datacube.nbytes*7,system=alternative)} of RAM...",
+            flush=True,
+        )
     else:
-        print(f"Unaccelerated TV denoising will require {size(datacube.nbytes*4,system=alternative)} of RAM...", flush=True)
+        print(
+            f"Unaccelerated TV denoising will require {size(datacube.nbytes*4,system=alternative)} of RAM...",
+            flush=True,
+        )
 
     calculate_error = reference_data is not None
     if calculate_error:
@@ -175,48 +237,59 @@ def denoise3D(datacube, lam, mu, iterations=75, BC_mode=2, FISTA=False, referenc
         d2 = np.zeros_like(datacube)
         d3 = np.zeros_like(datacube)
 
-        tk = 1.
+        tk = 1.0
 
     recon = datacube.copy()
 
     if FISTA:
-        for i in tqdm(range(int(iterations)), desc='FISTA Accelerated TV Denoising'):
+        for i in tqdm(range(int(iterations)), desc="FISTA Accelerated TV Denoising"):
             # update the tk factor
-            tk_new = (1 + np.sqrt(1 + 4*tk**2)) / 2
+            tk_new = (1 + np.sqrt(1 + 4 * tk ** 2)) / 2
             tk_ratio = tk / tk_new
             tk = tk_new
 
             # update accumulators
-            accumulator_update_3D_FISTA(recon, acc1, d1, tk_ratio, 0, lambdaInv[0], BC_mode=BC_mode)
-            accumulator_update_3D_FISTA(recon, acc2, d2, tk_ratio, 1, lambdaInv[1], BC_mode=BC_mode)
-            accumulator_update_3D_FISTA(recon, acc3, d3, tk_ratio, 2, lambdaInv[2], BC_mode=BC_mode)
+            accumulator_update_3D_FISTA(
+                recon, acc1, d1, tk_ratio, 0, lambdaInv[0], BC_mode=BC_mode
+            )
+            accumulator_update_3D_FISTA(
+                recon, acc2, d2, tk_ratio, 1, lambdaInv[1], BC_mode=BC_mode
+            )
+            accumulator_update_3D_FISTA(
+                recon, acc3, d3, tk_ratio, 2, lambdaInv[2], BC_mode=BC_mode
+            )
 
-            datacube_update_3D(datacube, recon, acc1, acc2, acc3, lam_mu, BC_mode=BC_mode)
+            datacube_update_3D(
+                datacube, recon, acc1, acc2, acc3, lam_mu, BC_mode=BC_mode
+            )
 
             if calculate_error:
-                MSE[i+1] = sum_square_error_3D(reference_data,recon)
+                MSE[i + 1] = sum_square_error_3D(reference_data, recon)
     else:
-        for i in tqdm(range(int(iterations)), desc='Unaccelerated TV Denoising'):
+        for i in tqdm(range(int(iterations)), desc="Unaccelerated TV Denoising"):
             # update accumulators
             accumulator_update_3D(recon, acc1, 0, lambdaInv[0], BC_mode=BC_mode)
             accumulator_update_3D(recon, acc2, 1, lambdaInv[1], BC_mode=BC_mode)
             accumulator_update_3D(recon, acc3, 2, lambdaInv[2], BC_mode=BC_mode)
 
             # update reconstruction
-            datacube_update_3D(datacube, recon, acc1, acc2, acc3, lam_mu, BC_mode=BC_mode)
+            datacube_update_3D(
+                datacube, recon, acc1, acc2, acc3, lam_mu, BC_mode=BC_mode
+            )
 
             if calculate_error:
-                MSE[i+1] = sum_square_error_3D(reference_data,recon)
+                MSE[i + 1] = sum_square_error_3D(reference_data, recon)
 
     if calculate_error:
         return recon, MSE
     else:
         return recon
 
+
 def check_memory(datacube):
-    '''
+    """
     Determine if there is sufficient RAM to perform different TV denoising algorithms
-    '''
+    """
     avail = psutil.virtual_memory().available
     dcsize = datacube.nbytes
 
@@ -232,13 +305,13 @@ def check_memory(datacube):
         algos = [
             ["Anisotropic Unaccelerated", fmt(dcsize * 5), checkmark(dcsize * 5)],
             ["Anisotropic FISTA", fmt(dcsize * 13), checkmark(dcsize * 13)],
-            ["(Half-)Isotropic Unaccelerated", fmt(dcsize * 5), checkmark(dcsize * 5)]
+            ["(Half-)Isotropic Unaccelerated", fmt(dcsize * 5), checkmark(dcsize * 5)],
         ]
     elif len(datacube.shape) == 3:
         algos = [
             ["Anisotropic Unaccelerated", fmt(dcsize * 4), checkmark(dcsize * 4)],
             ["Anisotropic FISTA", fmt(dcsize * 11), checkmark(dcsize * 11)],
-            ["(Half-)Isotropic Unaccelerated", fmt(dcsize * 5), checkmark(dcsize * 5)]
+            ["(Half-)Isotropic Unaccelerated", fmt(dcsize * 5), checkmark(dcsize * 5)],
         ]
 
     print(tabulate(algos, headers))
--- setup.py
+++ setup.py
@@ -1,52 +1,54 @@
 from setuptools import setup, Extension
+
 # from distutils.core import setup
 # from distutils.extension import Extension
 from Cython.Build import cythonize
 import platform
 import os
 
-if platform.system() == 'Windows':
-    extra_compile_args = ['/openmp']
-    extra_link_args = ['/openmp']
+if platform.system() == "Windows":
+    extra_compile_args = ["/openmp"]
+    extra_link_args = ["/openmp"]
 elif platform.system() == "Darwin":
-    extra_link_args = ['-fopenmp']
-    extra_compile_args = ['-fopenmp']
+    extra_link_args = ["-fopenmp"]
+    extra_compile_args = ["-fopenmp"]
 
     # we are on a Mac, link to the Homebrew installation of llvm
-    extra_link_args.append('-lgomp')
-    extra_link_args.append('-Wl,-rpath,/usr/local/opt/gcc/lib/gcc/9/')
+    extra_link_args.append("-lgomp")
+    extra_link_args.append("-Wl,-rpath,/usr/local/opt/gcc/lib/gcc/9/")
     # use the Homebrew gcc
-    os.environ['CC'] = 'gcc-9'
+    os.environ["CC"] = "gcc-9"
 else:
-    extra_compile_args = ['-fopenmp']
-    extra_link_args = ['-fopenmp']
+    extra_compile_args = ["-fopenmp"]
+    extra_link_args = ["-fopenmp"]
 
 # NOTE: On NERSC Cori things are harder:
 # What worked for me is to run `module swap PrgEnv-intel PrgEnv-cray`
 # to use the Cray compilers and also specify the compiler
 # when running setup.py: `CC='cc' LDSHARED='cc -shared' python setup.py build_ext`
 
-ext_modules = [Extension(
-    'cyTV4D.utils', ['cyTV4D/utils.pyx'],
-    extra_compile_args=extra_compile_args,
-    extra_link_args=extra_link_args),
+ext_modules = [
     Extension(
-        'cyTV4D.anisotropic', ['cyTV4D/anisotropic.pyx'],
+        "cyTV4D.utils",
+        ["cyTV4D/utils.pyx"],
         extra_compile_args=extra_compile_args,
-        extra_link_args=extra_link_args)]
+        extra_link_args=extra_link_args,
+    ),
+    Extension(
+        "cyTV4D.anisotropic",
+        ["cyTV4D/anisotropic.pyx"],
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
+    ),
+]
 
 setup(
-    name='cyTV4D',
-    version='0.0.1',
+    name="cyTV4D",
+    version="0.0.1",
     author="SE Zeltmann",
     author_email="steven.zeltmann@lbl.gov",
-    packages=['cyTV4D'],
+    packages=["cyTV4D"],
     ext_modules=cythonize(ext_modules),
-    install_requires=[
-        'Cython',
-        'hurry.filesize',
-        'psutil',
-        'tabulate'
-    ],
-    setup_requires=['Cython']
+    install_requires=["Cython", "hurry.filesize", "psutil", "tabulate"],
+    setup_requires=["Cython"],
 )
